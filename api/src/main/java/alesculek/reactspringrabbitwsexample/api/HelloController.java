7f3196ac3c4ed1abe4b78013a985f5c96998c43b
Aleš Culek <ales.culek@gmail.com>
2018-10-28 21:31:01 +0100

    Create api with spring boot (according to getting started)

---
+++ api/src/main/java/alesculek/reactspringrabbitwsexample/api/HelloController.java
@@ -0,0 +1,14 @@
+package alesculek.reactspringrabbitwsexample.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/")
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
